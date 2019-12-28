75ab93c9a586cdac804a4c246e212d764641d0b6
Oskar Kogut <kogutoskar123@gmail.com>
2019-12-28 10:24:38 +0100

    Added tower icons

--- TowerDefence/Assets/Scripts/Game/Game.Gameplay/CamerMovement.cs
+++ TowerDefence/Assets/Scripts/Game/Game.Gameplay/CamerMovement.cs
@@ -7,12 +7,24 @@ namespace Game.Gameplay
     public class CamerMovement : MonoBehaviour
     {
         [SerializeField]
-        [Range(1,100)]
-        private float _speed = 10;
+        private CameraSO _cameraSO = null;
 
-        [SerializeField]
-        [Range(1,100)]
-        private float _scrollSpeed = 30;
+        private float _speed;
+        private float _scrollSpeed;
+        private int _lowClamp;
+        private int _highClamp;
+        private int _lowScrollClamp;
+        private int _highScrollClamp;
+
+        private void Start()
+        {
+            _speed = _cameraSO.Speed;
+            _scrollSpeed = _cameraSO.ScrollSpeed;
+            _lowClamp = _cameraSO.LowClamp;
+            _highClamp = _cameraSO.HighClamp;
+            _lowScrollClamp = _cameraSO.ScrollLowClamp;
+            _highScrollClamp = _cameraSO.ScrollHighClamp;
+        }
 
         private void Update()
         {
@@ -20,6 +32,9 @@ namespace Game.Gameplay
             float v = Input.GetAxis("Vertical");
             float z = Input.mouseScrollDelta.y;
             transform.Translate(new Vector3(h * _speed, v * _speed, z * _scrollSpeed) * Time.deltaTime);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, _lowClamp, _highClamp), 
+                Mathf.Clamp(transform.position.y, _lowScrollClamp, _highScrollClamp), 
+                Mathf.Clamp(transform.position.z, _lowClamp, _highClamp));
         }
     }
 }
---
+++ TowerDefence/Assets/Scripts/Game/Game.Gameplay/CameraSO.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Camera", fileName = "Camera")]
+    public class CameraSO : ScriptableObject
+    {
+        [SerializeField]
+        [Range(1,100)]
+        private int _speed = 10;
+        [SerializeField]
+        [Range(1, 100)]
+        private int _scrollSpeed = 50;
+        [SerializeField]
+        [Range(1,100)]
+        private int _lowClamp = 1;
+        [SerializeField]
+        [Range(1, 100)]
+        private int _highClamp = 100;
+        [SerializeField]
+        [Range(1, 100)]
+        private int _scrollLowClamp = 10;
+        [SerializeField]
+        [Range(1, 100)]
+        private int _scrollHighClamp = 100;
+
+        public int Speed { get => _speed; set => _speed = value; }
+        public int ScrollSpeed { get => _scrollSpeed; set => _scrollSpeed = value; }
+        public int LowClamp { get => _lowClamp; set => _lowClamp = value; }
+        public int HighClamp { get => _highClamp; set => _highClamp = value; }
+        public int ScrollLowClamp { get => _scrollLowClamp; set => _scrollLowClamp = value; }
+        public int ScrollHighClamp { get => _scrollHighClamp; set => _scrollHighClamp = value; }
+    }
+}
