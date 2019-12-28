957cccf4378a9f9e7d4c087c14eccf5999724727
Oskar Kogut <kogutoskar123@gmail.com>
2019-12-28 07:21:03 +0100

    Added ground spawning

---
+++ TowerDefence/Assets/Scripts/Game/Game.Controllers/LevelGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Controllers
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelGeneratorSO _levelGeneratorSO;
+
+        private GameObject _parent;
+        private GameObject _ground;
+
+        private void Start()
+        {
+            _parent = new GameObject("Level");
+            _ground = Instantiate(_levelGeneratorSO.Ground, new Vector3(0,0), Quaternion.identity);
+            _ground.transform.localScale = new Vector3(_levelGeneratorSO.LevelWidth, 1, _levelGeneratorSO.LevelHeight);
+            _ground.transform.parent = _parent.transform;
+            _ground.name = "Ground";
+            _ground.transform.position = new Vector3(_levelGeneratorSO.LevelWidth / 2 + 0.5f, 0, _levelGeneratorSO.LevelHeight / 2 + 0.5f);
+        }
+    }
+}
---
+++ TowerDefence/Assets/Scripts/Game/Game.Controllers/LevelGeneratorSO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Controllers
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Level generator", fileName = "Level generator")]
+    public class LevelGeneratorSO : ScriptableObject
+    {
+        [SerializeField]
+        [Range(10,100)]
+        private int _levelWidth;
+
+        [SerializeField]
+        [Range(10, 100)]
+        private int _levelHeight;
+
+        [SerializeField]
+        private GameObject _ground;
+
+        [SerializeField]
+        private GameObject _pathTile;
+
+        public int LevelWidth { get => _levelWidth; set => _levelWidth = value; }
+        public int LevelHeight { get => _levelHeight; set => _levelHeight = value; }
+        public GameObject Ground { get => _ground; set => _ground = value; }
+        public GameObject PathTile { get => _pathTile; set => _pathTile = value; }
+    }
+}
